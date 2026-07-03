cb9a717c842a61217f202b9e3a957e5aa8dff29b
Sadaru Nandathissa <itt2023049@tec.rjt.ac.lk>
2026-07-03 14:09:34 +0530

    authroutes-verification

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { login } = require("../controllers/authController");
-
+const { protect } = require("../middleware/auth");
+//Public - Login
 router.post("/login", login);
 
+//Protected - Get current user
+router.get("/me", protect, getCurrentUser);
 module.exports = router;
