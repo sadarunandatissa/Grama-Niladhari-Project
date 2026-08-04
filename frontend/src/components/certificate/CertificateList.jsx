353bbac9d052285bb5639a87721a18d20ba7454e
Sadaru Nandathissa <itt2023049@tec.rjt.ac.lk>
2026-08-04 09:46:39 +0530

    call api for list

--- frontend/src/components/certificate/CertificateList.jsx
+++ frontend/src/components/certificate/CertificateList.jsx
@@ -2,4 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useAuth } from "../../context/AuthContext";
 
-const CertificateList = () => {};
+const CertificateList = () => {
+  const { token } = useAuth();
+  const [requests, setRequests] = useState([]);
+  const [loading, setLoading] = useState([true]);
+  const [error, setError] = useState("");
+
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+};
