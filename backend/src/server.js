5744ea00b545360d9629084a6c4c6c8c2cfa358c
Sadaru Nandathissa <itt2023049@tec.rjt.ac.lk>
2026-08-20 21:46:01 +0530

    update server

--- backend/src/server.js
+++ backend/src/server.js
@@ -83,6 +83,7 @@ app.use("/api/villages", require("../src/routes/villageRoutes"));
 app.use("/api/land", require("./routes/landRoutes"));
 app.use("/api/certificate", require("./routes/certificateRoutes"));
 app.use("/api/appointments", require("./routes/appointmentRoutes"));
+app.use("/api/announcements", require("./routes/announcementRoutes"));
 app.get("/api/health", (req, res) => {
   res.json({ success: true, message: "Server running" });
 });
